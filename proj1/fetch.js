26ebfb8d8334f1efa9a3feca630f7cd296a086ad
TapiocaGoku25 <mihirgokhale25@gmail.com>
2023-07-14 16:21:16 +0530

    Added fetch request

--- proj1/fetch.js
+++ proj1/fetch.js
@@ -1,15 +1,17 @@
-fetch('https://jsonplaceholder.typicode.com/todos%27')
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error('Network response was not OK');
-    }
-    return response.json();
-  })
-  .then((data) => {
-    // Process the received data
-    console.log(data);
-  })
-  .catch(error => {
-    // Handle any errors that occurred during the fetch request
-    console.log('Error:', error.message);
-  });
+fetch('https://jsonplaceholder.typicode.com/todos')
+    .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not OK');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        data.forEach((item)=>{
+          tasks.push({task:`${item.title}`});
+          ctr++;
+        })
+        console.log(data);
+      })
+      .catch(error => {
+        console.log('Error:', error.message);
+      });
